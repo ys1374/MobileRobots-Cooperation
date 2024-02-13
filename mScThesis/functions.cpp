0282c9daf92330619873c477db7f3ff7b338ccbd
Neo <salehi.yasin@gmail.com>
2024-02-13 04:51:00 +0330

    onGridRobotMovementCycleTime completed and first simplified cycletime is calculated

--- mScThesis/functions.cpp
+++ mScThesis/functions.cpp
@@ -25,7 +25,8 @@ void saveVectorToFile(const std::vector<Autostore::bin>& vec_, const std::string
     }
 
     for (const auto& item : vec_) {
-        fileOut << item << "\n";
+        //fileOut << robot.id << " " << robot.xLocation << " " << robot.yLocation << "\n";
+        fileOut << item.binId << "\n";
     }
 }
 
@@ -35,9 +36,10 @@ void loadVectorFromFile(std::vector<Autostore::bin>& queueOfBinRetrival_, const 
         throw std::runtime_error("Cannot open file for reading");
     }
 
-    Autostore::bin item;
-    while (fileIn >> item) {
-        queueOfBinRetrival_.push_back(binsVector_[item.binId]);
+    int binId;
+    while (fileIn >> binId) {
+        
+        queueOfBinRetrival_.push_back(binsVector_[binId]);
         //std::cout << item.binId;
     }
 }
@@ -51,18 +53,64 @@ bool fileExists(const std::string& filename) {
 void fillQueueOfBinRetrival(std::vector<Autostore::bin>& queueOfBinRetrival, const std::string& filename_, std::vector<Autostore::bin> binsVector_) {
     if (fileExists(filename_)) {
         // File exists, load vector from file
+        
         loadVectorFromFile(queueOfBinRetrival, filename_, binsVector_);
-        std::cout << "Loaded from file." << std::endl;
+        std::cout << "Loaded Queue from file." << std::endl;
     }
     else {
         Autostore::VectorShuffler<Autostore::bin> shuffler;
         queueOfBinRetrival = shuffler.shuffleVector(binsVector_);
-
+        //std::cout << "Test line\n";
         // Optionally, save the newly filled vector to file
         saveVectorToFile(queueOfBinRetrival, filename_);
-        std::cout << "Filled regularly and saved to file." << std::endl;
+        std::cout << "Queue Filled regularly and saved to file." << std::endl;
     }
 }
 
+
+///////for first robot-----------------------------------
+
+void saveRobotLocations(const std::vector<Autostore::firstRobot>& firstRobotsVector_, const std::string& filename_) {
+    std::ofstream fileOut(filename_);
+    if (!fileOut) {
+        throw std::runtime_error("Cannot open file for writing");
+    }
+
+    for (auto& robot : firstRobotsVector_) {
+        fileOut << robot.id << " " << robot.xLocation << " " << robot.yLocation << "\n";
+    }
+}
+
+
+void loadRobotLocations(std::vector<Autostore::firstRobot>& firstRobotsVector_, const std::string& filename) {
+    std::ifstream fileIn(filename);
+    if (!fileIn) {
+        throw std::runtime_error("Cannot open file for reading");
+    }
+
+    //robots.clear(); // Clear existing data
+    int x, y, id;
+
+    while (fileIn >> id >> x >> y) {
+        firstRobotsVector_[id].xLocation = x;
+        firstRobotsVector_[id].yLocation = y;
+        //robots.emplace_back(x, y, id);
+        //std::cout << id;
+    }
+}
+
+
+void fillFirstRobotLocation(std::vector<Autostore::firstRobot>& firstRobotsVector_, const std::string& filename_) {
+    if (fileExists(filename_)) {
+        // File exists, load vector from file
+        loadRobotLocations(firstRobotsVector_, filename_);
+        std::cout << "Loaded robots location from file." << std::endl;
+    }
+    else {
+        // Optionally, save the newly filled vector to file
+        saveRobotLocations(firstRobotsVector_, filename_);
+        std::cout << "Filled robots regularly and saved to file." << std::endl;
+    }
+}
 
 
